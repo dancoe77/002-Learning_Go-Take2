edd5877438a79f090165dc0090da544f3ef25e76
dancoe77 <dan@shoeman-tech.com>
2024-01-15 12:28:02 -0800

    Module 14 completed

---
+++ 04-The_fundamentals_of_Go/014-Using_printf_for_decimal_and_hex_values/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main(){
+	adams := 42
+	fmt.Printf("42 as binary, %b \n", adams)
+	fmt.Printf("42 as hexadecimal, %x \n", adams)
+	
+	//print these values as both binary & hex
+	a, b, c,d, e, f := 0, 1, 2, 3, 4, 5
+	fmt.Println("Value \t Binary  Hex")
+	fmt.Printf("%v \t %b \t %#x \t \n",a,a,a)
+	fmt.Printf("%v \t %b \t %#x \t \n",b,b,b)
+	fmt.Printf("%v \t %b \t %#x \t \n",c,c,c)
+	fmt.Printf("%v \t %b \t %#x \t \n",d,d,d)
+	fmt.Printf("%v \t %b \t %#x \t \n",e,e,e)
+	fmt.Printf("%v \t %b \t %#x \t \n",f,f,f)
+	
+}
