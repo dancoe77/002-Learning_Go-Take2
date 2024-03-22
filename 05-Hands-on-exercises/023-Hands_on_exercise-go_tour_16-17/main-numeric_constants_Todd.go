27afcd5a9433a033ffc9c8e5781141510d746dc2
dancoe77 <dan@shoeman-tech.com>
2024-03-22 11:08:56 -0700

    completed module 024

--- 05-Hands-on-exercises/023-Hands_on_exercise-go_tour_16-17/main-numeric_constants_Todd.go
+++ 05-Hands-on-exercises/023-Hands_on_exercise-go_tour_16-17/main-numeric_constants_Todd.go
@@ -2,14 +2,25 @@ package main
 
 import "fmt"
 
+const (
+	_ = iota // c0 == 0
+	b
+	c
+	d
+	e
+	f
+	g
+	h
+)
+
 func main() {
 	fmt.Printf("%d \t %b\n", 1, 1)
-	fmt.Printf("%d \t %b\n", 1<<1, 1<<1)
-	fmt.Printf("%d \t %b\n", 1<<2, 1<<2)
-	fmt.Printf("%d \t %b\n", 1<<3, 1<<3)
-	fmt.Printf("%d \t %b\n", 1<<4, 1<<4)
-	fmt.Printf("%d \t %b\n", 1<<5, 1<<5)
-	fmt.Printf("%d \t %b\n", 1<<6, 1<<6)
-	fmt.Printf("%d \t %b\n", 1<<7, 1<<7)
-	fmt.Printf("%d \t %b\n", 1<<8, 1<<8)
+	fmt.Printf("%d \t %b\n", 1<<a, 1<<a)
+	fmt.Printf("%d \t %b\n", 1<<b, 1<<b)
+	fmt.Printf("%d \t %b\n", 1<<c, 1<<c)
+	fmt.Printf("%d \t %b\n", 1<<d, 1<<d)
+	fmt.Printf("%d \t %b\n", 1<<e, 1<<e)
+	fmt.Printf("%d \t %b\n", 1<<f, 1<<f)
+	fmt.Printf("%d \t %b\n", 1<<g, 1<<g)
+	fmt.Printf("%d \t %b\n", 1<<h, 1<<h)
 }
---
+++ 05-Hands-on-exercises/024-Hands_on_exercise-iota/main-iota.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("%d \t %b\n", 1, 1)
+	fmt.Printf("%d \t %b\n", 1<<1, 1<<1)
+	fmt.Printf("%d \t %b\n", 1<<2, 1<<2)
+	fmt.Printf("%d \t %b\n", 1<<3, 1<<3)
+	fmt.Printf("%d \t %b\n", 1<<4, 1<<4)
+	fmt.Printf("%d \t %b\n", 1<<5, 1<<5)
+	fmt.Printf("%d \t %b\n", 1<<6, 1<<6)
+	fmt.Printf("%d \t %b\n", 1<<7, 1<<7)
+	fmt.Printf("%d \t %b\n", 1<<8, 1<<8)
+}
