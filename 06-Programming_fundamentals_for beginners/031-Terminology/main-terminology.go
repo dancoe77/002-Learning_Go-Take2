860f45ed227ffdacd725c3143dc7435a29d4e541
dancoe77 <dan@shoeman-tech.com>
2024-03-22 23:20:54 -0700

    completed module 031

---
+++ 06-Programming_fundamentals_for beginners/031-Terminology/main-terminology.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main(){
+	
+	var w int // DECLARE a variable to hold a VALUE of a certain TYPE
+	fmt.Println(w)
+	
+	var x int = 69 //assign a value to a variable
+	fmt.Println(x)
+	
+	var y int //initialize
+	y = 42
+	fmt.Println(y)
+	
+	z := 2+7 //statement and expression
+	fmt.Println(z)
+}
