b384f727a9ce49cec63c21d000cb6ca0b1ca9c9b
dancoe77 <dan@shoeman-tech.com>
2024-03-23 15:55:25 -0700

    completed module 35 & 36

---
+++ 06-Programming_fundamentals_for beginners/035-Terminal_commands_part1/main-part1.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+pwd
+
+ls
+
+ls -la
+
+clear
+
+cd /home/dan
---
+++ 06-Programming_fundamentals_for beginners/036-Terminal_commands_part2/main-part2.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+cd Software
+
+cd ..
+
+mkdir Code
+
+touch temp.txt
+
+nano temp.txt
+
+cat temp.txt
+
+clear
+
+chmod 755 temp.txt
+
+env
+
+go version
+
+go env
+
+rm temp.txt
+
+rm -rf Code/
