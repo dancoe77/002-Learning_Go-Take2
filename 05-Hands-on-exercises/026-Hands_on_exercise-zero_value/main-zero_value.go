279bb3a9fbd9b3a7b49fe7b2537f0e3a9f29ef1d
dancoe77 <dan@shoeman-tech.com>
2024-03-22 18:17:49 -0700

    completed module 026

---
+++ 05-Hands-on-exercises/026-Hands_on_exercise-zero_value/main-zero_value.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main(){
+	var a int
+	fmt.Println(a)
+	
+	b, c, d, _, f := 0, 1, 2, 3, "happiness"
+	fmt.Println(b, c, d, f)
+	
+	var g int = 69
+	fmt.Println(g)
+}
+
