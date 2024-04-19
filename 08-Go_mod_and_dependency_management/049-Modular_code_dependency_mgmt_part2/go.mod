7b1df8b76caf0ee92c7e6cf8c2d4c3b1472bcfae
dancoe77 <dan@shoeman-tech.com>
2024-04-19 11:56:37 -0700

    looking for help

---
+++ 08-Go_mod_and_dependency_management/049-Modular_code_dependency_mgmt_part2/go.mod
@@ -0,0 +1,5 @@
+module github.com/dancoe77/008-puppy
+
+go 1.21.9
+
+require github.com/dancoe77/009-dog v1.0.0
---
+++ 08-Go_mod_and_dependency_management/049-Modular_code_dependency_mgmt_part2/main.go
@@ -0,0 +1,21 @@
+package main
+
+import(
+	"fmt"
+	"github.com/dancoe77/008-puppy"
+	"github.com/dancoe77/009-dog"
+)
+func main(){
+	s1 := puppy.Bark()
+	s2 := puppy.Barks()
+	fmt.Println(s1)
+	fmt.Println(s2)
+	
+	s3 := dog.BigBark()
+	s4 := dog.BigBarks()
+	fmt.Println(s3)
+	fmt.Println(s4)
+	
+	//also like this
+	
+}
