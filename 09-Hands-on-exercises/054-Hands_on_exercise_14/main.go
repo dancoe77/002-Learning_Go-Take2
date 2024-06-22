6def6faddaa1edd1e456fc224386138e951455cf
Dan S <dan@shoeman-tech.com>
2024-06-21 19:50:16 -0700

    completed module 054

---
+++ 09-Hands-on-exercises/054-Hands_on_exercise_14/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+var Fedora string = "I am Fedora"
+
+const Versionfedora int = 40
+
+func main(){
+	s1 := Fedora
+	s2 := Versionfedora
+	fmt.Println(s1,s2)
+}
