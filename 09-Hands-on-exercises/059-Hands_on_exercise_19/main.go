693dd7720ec889a0a57a6ce5a92a6d23956716cd
Dan S <dan@shoeman-tech.com>
2024-06-21 23:43:48 -0700

    initial commit for 059

---
+++ 09-Hands-on-exercises/059-Hands_on_exercise_19/main.go
@@ -0,0 +1,15 @@
+package main
+
+import(
+	"fmt"
+	"github.com/dancoe77/010-version"
+)
+
+func main(){
+	
+	t1 := version.Fedora
+	t2 := version.Versionlinux
+	t3 := version.Versionlinux
+	fmt.Println(t1, t3)
+	fmt.Println(t2)
+}
