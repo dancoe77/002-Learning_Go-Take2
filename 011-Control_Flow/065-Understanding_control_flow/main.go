a47f4ccd17efcf0da532c393609fcc0c55307ef5
Dan S <dan@shoeman-tech.com>
2024-07-07 23:34:23 -0700

    completed module 068

--- 011-Control_Flow/065-Understanding_control_flow/main.go
+++ 011-Control_Flow/065-Understanding_control_flow/main.go
@@ -95,9 +95,9 @@ func main() {
 	// https://go.dev/play/p/OXGzjxVkag0
 	
 	if z := 2 * rand.Intn(x); z>= x {
-		fmt.Printf("z is %v and that is GREATER THAN OR EQUAL x which is %v/n", z, x)
+		fmt.Printf("z is %v and that is GREATER THAN OR EQUAL x which is %v /n", z, x)
 	} else {
-		fmt.Printf("z is %v and that is LESS THAN x which is %v/n", z, x)
+		fmt.Printf("z is %v and that is LESS THAN x which is %v /n", z, x)
 	}
 	
 	switch {
