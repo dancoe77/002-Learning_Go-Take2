ee16d510bbe476aa8e47e7f73dadfa6442d8f913
Dan S <dan@shoeman-tech.com>
2024-08-03 22:08:00 -0700

    completed module 092

--- 012-Hands-on-exercises/089-Hands_on_exercise_37/main.go
+++ 012-Hands-on-exercises/089-Hands_on_exercise_37/main.go
@@ -8,22 +8,22 @@ func main (){
 	var bond = map[string]int{
 		"James":		42,
 		"Moneypenny":	32,
-		"Q":			22,
+		//"Q":			52,
 	}
 	for k, v := range bond {
 		fmt.Println("ranging over a map", k, v)
 	}
 	m1 := bond["James"]
-	fmt.Println(m1)
+	fmt.Println("The age of Bond is", m1)
 	
+	if v, ok := bond["James"]; ok {
+		fmt.Println("There is a Bond lookup entry, and Bond's age is", v)
+	}
+
 	m2 := bond["Q"]
-	fmt.Println(m2)
+	fmt.Println("The age of Q is", m2)
 	
-	func m2(Q string) int {
-    if seconds, ok := timeZone[tz]; ok {
-        return seconds
-    }
-    log.Println("unknown time zone:", tz)
-    return 0
+	if v, ok := bond["Q"]; !ok {
+		fmt.Println("There is no Q, and here is the zero value of an int", v)
 	}
 }
