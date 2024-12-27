8fd7ec1a37f11f4014d45d6b6cfb93c4a5377df4
Dan Shoemaker <dan@shoeman-tech.com>
2024-12-26 22:55:41 -0800

    completed module 110

---
+++ 014-Hands_on_exercises/110-Hands_on_exercise_44/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+	xi := []int{42, 43, 44, 45, 46, 47, 48, 49, 50, 51}
+	var s string = "#####################################"
+	fmt.Println(s)
+	fmt.Println(xi)
+	fmt.Printf("%T\n", xi)
+	fmt.Println(len(xi))
+	fmt.Println(cap(xi))
+	fmt.Println(s)
+
+	// blank identifier to not use a returned value
+	for k, v := range xi {
+		fmt.Printf("%v\t %v\t %T\n", k, v, v)
+	}
+	fmt.Println(s)
+
+	// slicing a slice
+	xs := []int{42, 43, 44, 45, 46, 47, 48, 49, 50, 51}
+	fmt.Printf("xs - %#v\n", xs[:5])
+	fmt.Println(s)
+
+	fmt.Printf("xs - %#v\n", xs[5:])
+	fmt.Println(s)
+
+	fmt.Printf("xs - %#v\n", xs[2:7])
+	fmt.Println(s)
+
+	fmt.Printf("xs - %#v\n", xs[1:7])
+	fmt.Println(s)
+}
