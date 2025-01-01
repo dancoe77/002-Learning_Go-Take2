30210712a702802712d70cf4f8e9171d99b7227d
Dan Shoemaker <dan@shoeman-tech.com>
2024-12-31 19:58:23 -0800

    completed module 120

---
+++ 015-Grouping_data_values_map/120-Map_counting_words_in_a_book/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 0
+	fmt.Println(a)
+	a++
+	fmt.Println(a)
+
+	m := make(map[string]int)
+	fmt.Println(m["beautiful"])
+	m["beautiful"]++
+	fmt.Println(m["beautiful"])
+	m["beautiful"]++
+	fmt.Println(m["beautiful"])
+	m["beautiful"]++
+	fmt.Println(m["beautiful"])
+}
