77d838f1aed48ba9d26e970a60748acdd08cd3e6
Dan Shoemaker <dan@shoeman-tech.com>
2024-12-31 20:40:04 -0800

    completed module 121

---
+++ 016-Hands-on-exercises/121-Hands_on_exercise_49/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	am := make(map[string][]string)
+	am["bond_james"] = []string{"shaken, not stirred", "martinis", "fast cars"}
+	am["moneypenny_jenny"] = []string{"intelligence", "literature", "computer science"}
+	am["no_dr"] = []string{"cats", "ice cream", "sunsets"}
+
+	fmt.Println(am)
+	fmt.Printf("%#v\n", am)
+	fmt.Println(len(am))
+
+	for k, v := range am {
+		fmt.Println(k)
+		for i, v2 := range v {
+			fmt.Println(i, v2)
+		}
+	}
+}
