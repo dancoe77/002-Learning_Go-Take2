86965e6fd7d2623a236cc3d3057e3eb2cfe67779
Dan Shoemaker <dan@shoeman-tech.com>
2025-02-06 20:05:10 -0800

    completed module 133

---
+++ 019-Functions_in_Go/133-Intro_to_functions/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+/*
+	Intro to modular code
+		- spaghetti code
+		- structured/procedural code
+			- purpose
+				- "abstract" code
+				- code reusability
+				- more understandable
+			- functions
+			- packages
+	*/
+}
