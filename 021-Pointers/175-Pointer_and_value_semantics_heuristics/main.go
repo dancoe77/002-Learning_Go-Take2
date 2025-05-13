f598010f81bd7fb93936b79668592a6551007c3c
Dan Shoemaker <dan@shoeman-tech.com>
2025-05-13 16:23:12 -0700

    completed module 175

---
+++ 021-Pointers/175-Pointer_and_value_semantics_heuristics/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// Use Value semantics by default
+
+	// Use pointer semantics for large data sets
+
+	// Use Pointer semantics for Mutability
+
+	// Consistency - once Pointer semantics have been used always use pointer semantics
+
+	// Use Pointer semantics when Interfacing with other code e.g a library or system call
+}
