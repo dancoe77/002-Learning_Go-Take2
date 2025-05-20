9ed5719578ddb22a28c4c8932d5f9f32679fd5b2
Dan Shoemaker <dan@shoeman-tech.com>
2025-05-20 15:26:23 -0700

    completed module 188

---
+++ 023-Generics/188-Concrete_type_vs_interface_type/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	//Concrete type = int, bool, string, float64, arrays, slices, maps, structs, etc...
+
+	//Interface types are abstract, they represent behavior or type but not a specific set of values
+}
