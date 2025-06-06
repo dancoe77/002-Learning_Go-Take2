e5999639a81a94764766b4039745a38fae7645d5
Dan Shoemaker <dan@shoeman-tech.com>
2025-06-06 12:11:33 -0700

    completed module 243

--- 032-Writing_documentation/243-godoc_org/main.go
+++ 032-Writing_documentation/243-godoc_org/main.go
@@ -3,5 +3,21 @@ package main
 import "fmt"
 
 func main() {
+	//to get a package on pkg.go.dev
+	/*
+	Adding a package
+	Data for the site is downloaded from proxy.golang.org. We monitor the Go Module Index regularly for new packages to add to pkg.go.dev. If you don’t see a package on pkg.go.dev, you can add it by doing one of the following:
+
+	Visiting that page on pkg.go.dev, and clicking the “Request” button. For example:
+	https://pkg.go.dev/example.com/my/module
+
+	Making a request to proxy.golang.org for the module version, to any endpoint specified by the Module proxy protocol. For example:
+	https://proxy.golang.org/example.com/my/module/@v/v1.0.0.info
+
+	Downloading the package via the go command. For example:
+	GOPROXY=https://proxy.golang.org GO111MODULE=on go get example.com/my/module@v1.0.0
+
+	https://pkg.go.dev/about#adding-a-package
+	*/
 	
 }
