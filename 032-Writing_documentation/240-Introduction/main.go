07abf892771dd7bf4dfc4242cc82d372fc020278
Dan Shoemaker <dan@shoeman-tech.com>
2025-06-06 11:13:58 -0700

    completed module 240

---
+++ 032-Writing_documentation/240-Introduction/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	//godoc.org redirects to pkg.go.dev and it used to contain both the standard library and third-party applications
+
+	//golang.org redirects to go.dev
+
+	//The godoc command allows us to extract and generate documentation for Go packages
+
+	//The go doc command will show documentation for a package or symbol
+
+}
