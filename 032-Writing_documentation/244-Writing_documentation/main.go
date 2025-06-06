2b49b1d4ac65fd62c497f2e77cfb098fe1994c8b
Dan Shoemaker <dan@shoeman-tech.com>
2025-06-06 12:40:42 -0700

    completed module 244

---
+++ 032-Writing_documentation/244-Writing_documentation/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	//When a big section needs to be added use doc.go and then /* */ to add a large section at the beginning of the documentation
+
+	//Godoc: documenting Go code
+	//https://go.dev/blog/godoc
+
+}
