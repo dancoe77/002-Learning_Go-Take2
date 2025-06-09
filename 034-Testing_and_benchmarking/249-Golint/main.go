caf7f597c4f684cb79771baf779c8fb162d3fabf
Dan Shoemaker <dan@shoeman-tech.com>
2025-06-08 22:48:24 -0700

    completed module 249

---
+++ 034-Testing_and_benchmarking/249-Golint/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	//NOTE: Golint is deprecated and frozen. There's no drop-in replacement for it, but tools such as Staticcheck and go vet should be used instead.
+
+	//Golint is a linter for Go source code.
+
+
+}
