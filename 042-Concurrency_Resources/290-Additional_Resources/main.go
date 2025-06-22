f3f301867082826ac25a2ab62b9c17a59682636a
Dan Shoemaker <dan@shoeman-tech.com>
2025-06-21 22:48:45 -0700

    completed module 290

---
+++ 042-Concurrency_Resources/290-Additional_Resources/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	// https://github.com/ardanlabs/gotraining
+
+	// https://go.dev/blog/
+
+	// https://go.dev/doc/effective_go
+
+	// https://gobyexample.com/
+}
