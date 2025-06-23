111ba6dcba8d3c9e5fce2606d8a1ed13983fe7c9
Dan Shoemaker <dan@shoeman-tech.com>
2025-06-22 19:21:36 -0700

    completed module 291

---
+++ 043-Error_Handling/291-Intro_to_Error_Handling_in_Go/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	// https://go.dev/doc/faq
+
+	// https://en.wikipedia.org/wiki/Exception_handling#Criticism
+
+	// https://en.wikipedia.org/wiki/Exception_handling_(programming)#Criticism
+}
