12e063b4fc8692945a9c53328a95aa4016f49cac
Dan Shoemaker <dan@shoeman-tech.com>
2025-06-22 21:44:07 -0700

    completed module 299

---
+++ 043-Error_Handling/299-Error_Handling_Review/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("https://go.dev/doc/effective_go#errors")
+	fmt.Println("https://www.ardanlabs.com/blog/2014/10/error-handling-in-go-part-i.html")
+	fmt.Println("https://www.ardanlabs.com/blog/2014/11/error-handling-in-go-part-ii.html")
+	fmt.Println("https://go.dev/blog/error-handling-and-go")
+	fmt.Println("https://pkg.go.dev/errors@go1.24.4#pkg-overview")
+}
